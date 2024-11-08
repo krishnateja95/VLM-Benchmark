b28ed6c5e6fba0ebd8a656467ca65b6843ab7ee7
krishnateja95 <krishnateja95@outlook.com>
2024-11-08 21:08:09 +0000

    VLM Results

--- Performance/vLLM/run.sh
+++ Performance/vLLM/run.sh
@@ -1,10 +1,37 @@
-module use /soft/modulefiles/
-module load conda
-conda activate trt_llm_vllm
+#!/bin/bash -l
+#PBS -l select=1
+#PBS -l filesystems=home:eagle:grand
+#PBS -l walltime=30:00:00
+#PBS -q preemptable
+#PBS -A datascience
 
-# for model in "llava" "llava-next" "llava-onevision" "fuyu" "phi3_v" "paligemma" "chameleon" "minicpmv" "blip-2" "internvl_chat" "NVLM_D" "qwen_vl" "qwen2_vl" "mllama"; do
-for model in "mllama"; do
-    python3 vllm_infer.py --model-type=$model --num-prompts=4 --modality="image"
-done
+# module use /soft/modulefiles/
+# module load conda
+# conda activate trt_llm_vllm
+
+# export TMPDIR="/lus/grand/projects/datascience/krishnat/tmp_dir/"
+# export RAY_TMPDIR='/tmp'
+
+# cd /lus/grand/projects/datascience/krishnat/home_dir_code/Active_projects/VLM_Inference_Bench/VLM-Benchmark/Performance/vLLM/
+
+# for model in "mllama" "pixtral_hf" "molmo" "qwen2_vl" "llava" "phi3_v"; do
+#     for tensor_parallel in 1 2 4; do
+#         for batch_size in 1 16 32 64 128; do
+#             for image_size in 128 256 512 1024; do
+#                 for input_output_length in 128 256 512 1024 2048; do
+#                     python3 vllm_infer.py --model-type=$model --num-prompts=$batch_size --max-input-len=$input_output_length --max-new-tokens=$input_output_length --image-size=$image_size --num-gpus=$tensor_parallel --modality="image"
+#                 done
+#             done
+#         done
+#     done
+# done
 
 
+
+cp mllama_infer.py chameleon_infer.py
+cp mllama_infer.py llava_infer.py
+cp mllama_infer.py molmo_infer.py
+cp mllama_infer.py paligemma_infer.py
+cp mllama_infer.py phi3v_infer.py
+cp mllama_infer.py pixtral_infer.py
+cp mllama_infer.py qwen2vl_infer.py
--- Performance/vLLM/vllm_infer.py
+++ Performance/vLLM/vllm_infer.py
@@ -14,307 +14,157 @@ import os, csv
 cache_dir = '/lus/grand/projects/datascience/krishnat/model_weights/LLaMA/llama_cache/'
 
 # LLaVA-1.5
-def run_llava(question: str, modality: str):
+def run_llava(modality: str, num_gpus: int):
     assert modality == "image"
+    model_name = "llava-hf/llava-1.5-7b-hf"
+    llm = LLM(model="llava-hf/llava-1.5-7b-hf", max_model_len=4096, download_dir=cache_dir, tensor_parallel_size=num_gpus)
+    return llm, model_name
 
+def get_llava_data(question: str):
     prompt = f"USER: <image>\n{question}\nASSISTANT:"
-
-    llm = LLM(model="llava-hf/llava-1.5-7b-hf", max_model_len=4096, download_dir=cache_dir)
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
-
-
-# LLaVA-1.6/LLaVA-NeXT
-def run_llava_next(question: str, modality: str):
-    assert modality == "image"
-
-    prompt = f"[INST] <image>\n{question} [/INST]"
-    llm = LLM(model="llava-hf/llava-v1.6-mistral-7b-hf", max_model_len=8192, download_dir=cache_dir)
-    stop_token_ids = None
-    return llm, prompt, stop_token_ids
-
-
-# LlaVA-NeXT-Video
-# Currently only support for video input
-def run_llava_next_video(question: str, modality: str):
-    assert modality == "video"
-
-    prompt = f"USER: <video>\n{question} ASSISTANT:"
-    llm = LLM(model="llava-hf/LLaVA-NeXT-Video-7B-hf", max_model_len=8192, download_dir=cache_dir)
-    stop_token_ids = None
-    return llm, prompt, stop_token_ids
-
-
-# LLaVA-OneVision
-def run_llava_onevision(question: str, modality: str):
-
-    if modality == "video":
-        prompt = f"<|im_start|>user <video>\n{question}<|im_end|> \
-        <|im_start|>assistant\n"
-
-    elif modality == "image":
-        prompt = f"<|im_start|>user <image>\n{question}<|im_end|> \
-        <|im_start|>assistant\n"
-
-    llm = LLM(model="llava-hf/llava-onevision-qwen2-7b-ov-hf",
-              max_model_len=16384, download_dir=cache_dir)
-    stop_token_ids = None
-    return llm, prompt, stop_token_ids
-
-
-# Fuyu
-def run_fuyu(question: str, modality: str):
-    assert modality == "image"
-
-    prompt = f"{question}\n"
-    llm = LLM(model="adept/fuyu-8b", max_model_len=2048, max_num_seqs=2, download_dir=cache_dir)
-    stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
 
 
 # Phi-3-Vision
-def run_phi3v(question: str, modality: str):
+def run_phi3v(modality: str, num_gpus: int):
     assert modality == "image"
-
-    prompt = f"<|user|>\n<|image_1|>\n{question}<|end|>\n<|assistant|>\n"  # noqa: E501
-    # Note: The default setting of max_num_seqs (256) and
-    # max_model_len (128k) for this model may cause OOM.
-    # You may lower either to run this example on lower-end GPUs.
-
-    # In this example, we override max_num_seqs to 5 while
-    # keeping the original context length of 128k.
-
-    # num_crops is an override kwarg to the multimodal image processor;
-    # For some models, e.g., Phi-3.5-vision-instruct, it is recommended
-    # to use 16 for single frame scenarios, and 4 for multi-frame.
-    #
-    # Generally speaking, a larger value for num_crops results in more
-    # tokens per image instance, because it may scale the image more in
-    # the image preprocessing. Some references in the model docs and the
-    # formula for image tokens after the preprocessing
-    # transform can be found below.
-    #
-    # https://huggingface.co/microsoft/Phi-3.5-vision-instruct#loading-the-model-locally
-    # https://huggingface.co/microsoft/Phi-3.5-vision-instruct/blob/main/processing_phi3_v.py#L194
+    model_name = "microsoft/Phi-3-vision-128k-instruct"
     llm = LLM(
-        model="microsoft/Phi-3-vision-128k-instruct",
+        model=model_name,
         trust_remote_code=True,
         max_model_len=4096,
         max_num_seqs=2,
         download_dir=cache_dir,
-        # Note - mm_processor_kwargs can also be passed to generate/chat calls
         mm_processor_kwargs={"num_crops": 16},
+        tensor_parallel_size=num_gpus
     )
+    return llm, model_name
+
+def get_phi3v_data(question: str):
+    prompt = f"<|user|>\n<|image_1|>\n{question}<|end|>\n<|assistant|>\n"  # noqa: E501
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
 
 
 # PaliGemma
-def run_paligemma(question: str, modality: str):
+def run_paligemma(modality: str, num_gpus: int):
     assert modality == "image"
+    model_name = "google/paligemma-3b-mix-224"
+    llm = LLM(model=model_name, download_dir=cache_dir, tensor_parallel_size=num_gpus)
+    return llm, model_name
 
-    # PaliGemma has special prompt format for VQA
+def get_paligemma_data(question: str):
     prompt = "caption en"
-    llm = LLM(model="google/paligemma-3b-mix-224", download_dir=cache_dir)
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
+
 
 
 # Chameleon
-def run_chameleon(question: str, modality: str):
+def run_chameleon(modality: str, num_gpus: int):
     assert modality == "image"
+    model_name = "facebook/chameleon-7b"
+    llm = LLM(model=model_name, max_model_len=4096, download_dir=cache_dir, tensor_parallel_size=num_gpus)
+    return llm, model_name
 
+def get_chameleon_data(question: str):
     prompt = f"{question}<image>"
-    llm = LLM(model="facebook/chameleon-7b", max_model_len=4096, download_dir=cache_dir)
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
 
 
-# MiniCPM-V
-def run_minicpmv(question: str, modality: str):
+# Pixtral HF-format
+def run_pixtral_hf(modality: str, num_gpus: int):
     assert modality == "image"
+    model_name = "mistral-community/pixtral-12b"
+    llm = LLM(model=model_name, max_model_len=8192, download_dir=cache_dir, tensor_parallel_size=num_gpus)
+    return llm, model_name
 
-    # 2.0
-    # The official repo doesn't work yet, so we need to use a fork for now
-    # For more details, please see: See: https://github.com/vllm-project/vllm/pull/4087#issuecomment-2250397630 # noqa
-    # model_name = "HwwwH/MiniCPM-V-2"
-
-    # 2.5
-    # model_name = "openbmb/MiniCPM-Llama3-V-2_5"
-
-    #2.6
-    model_name = "openbmb/MiniCPM-V-2_6"
-    tokenizer = AutoTokenizer.from_pretrained(model_name,
-                                              trust_remote_code=True)
-    llm = LLM(
-        model=model_name,
-        max_model_len=4096,
-        max_num_seqs=2,
-        trust_remote_code=True, download_dir=cache_dir
-    )
-    # NOTE The stop_token_ids are different for various versions of MiniCPM-V
-    # 2.0
-    # stop_token_ids = [tokenizer.eos_id]
-
-    # 2.5
-    # stop_token_ids = [tokenizer.eos_id, tokenizer.eot_id]
-
-    # 2.6
-    stop_tokens = ['<|im_end|>', '<|endoftext|>']
-    stop_token_ids = [tokenizer.convert_tokens_to_ids(i) for i in stop_tokens]
-
-    messages = [{
-        'role': 'user',
-        'content': f'(<image>./</image>)\n{question}'
-    }]
-    prompt = tokenizer.apply_chat_template(messages,
-                                           tokenize=False,
-                                           add_generation_prompt=True)
-    return llm, prompt, stop_token_ids
-
-
-# InternVL
-def run_internvl(question: str, modality: str):
-    assert modality == "image"
-
-    model_name = "OpenGVLab/InternVL2-2B"
-
-    llm = LLM(
-        model=model_name,
-        trust_remote_code=True,
-        max_model_len=4096,download_dir=cache_dir
-    )
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name,
-                                              trust_remote_code=True)
-    messages = [{'role': 'user', 'content': f"<image>\n{question}"}]
-    prompt = tokenizer.apply_chat_template(messages,
-                                           tokenize=False,
-                                           add_generation_prompt=True)
-
-    # Stop tokens for InternVL
-    # models variants may have different stop tokens
-    # please refer to the model card for the correct "stop words":
-    # https://huggingface.co/OpenGVLab/InternVL2-2B#service
-    stop_tokens = ["<|endoftext|>", "<|im_start|>", "<|im_end|>", "<|end|>"]
-    stop_token_ids = [tokenizer.convert_tokens_to_ids(i) for i in stop_tokens]
-    return llm, prompt, stop_token_ids
-
-
-# NVLM-D
-def run_nvlm_d(question: str, modality: str):
-    assert modality == "image"
-
-    model_name = "nvidia/NVLM-D-72B"
-
-    # Adjust this as necessary to fit in GPU
-    llm = LLM(
-        model=model_name,
-        trust_remote_code=True,
-        max_model_len=4096,
-        tensor_parallel_size=4,download_dir=cache_dir
-    )
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name,
-                                              trust_remote_code=True)
-    messages = [{'role': 'user', 'content': f"<image>\n{question}"}]
-    prompt = tokenizer.apply_chat_template(messages,
-                                           tokenize=False,
-                                           add_generation_prompt=True)
+def get_pixtral_hf_data(question: str):
+    prompt = f"<s>[INST]{question}\n[IMG][/INST]"
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
 
 
-# BLIP-2
-def run_blip2(question: str, modality: str):
+# Molmo
+def run_molmo(modality, num_gpus: int):
     assert modality == "image"
+    model_name = "allenai/Molmo-7B-D-0924"
+    llm = LLM(model=model_name, trust_remote_code=True, dtype="bfloat16", tensor_parallel_size=num_gpus)
+    return llm, model_name
 
-    # BLIP-2 prompt format is inaccurate on HuggingFace model repository.
-    # See https://huggingface.co/Salesforce/blip2-opt-2.7b/discussions/15#64ff02f3f8cf9e4f5b038262 #noqa
-    prompt = f"Question: {question} Answer:"
-    llm = LLM(model="Salesforce/blip2-opt-2.7b", download_dir=cache_dir)
+def get_molmo_data(question):
+    prompt = question
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
-
-
-# Qwen
-def run_qwen_vl(question: str, modality: str):
-    assert modality == "image"
-
-    llm = LLM(
-        model="Qwen/Qwen-VL",
-        trust_remote_code=True,
-        max_model_len=1024,
-        max_num_seqs=2,download_dir=cache_dir
-    )
-
-    prompt = f"{question}Picture 1: <img></img>\n"
-    stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
 
 
 # Qwen2-VL
-def run_qwen2_vl(question: str, modality: str):
+def run_qwen2_vl(modality: str, num_gpus: int):
     assert modality == "image"
-
     model_name = "Qwen/Qwen2-VL-7B-Instruct"
+    llm = LLM(model=model_name, max_model_len=8192, max_num_seqs=5,download_dir=cache_dir, tensor_parallel_size=num_gpus)
+    return llm, model_name
 
-    # Tested on L40
-    llm = LLM(
-        model=model_name,
-        max_model_len=8192,
-        max_num_seqs=5,download_dir=cache_dir
-    )
-
+def get_qwen2_vl_data(question: str):
     prompt = ("<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n"
               "<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>"
               f"{question}<|im_end|>\n"
               "<|im_start|>assistant\n")
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
+
 
 
 # LLama 3.2
-def run_mllama(question: str, modality: str):
+def run_mllama(modality: str, num_gpus: int):
     assert modality == "image"
-
     model_name = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+    llm = LLM(model=model_name, max_model_len=4096, max_num_seqs=16, enforce_eager=True, download_dir=cache_dir, tensor_parallel_size=num_gpus)
+    return llm, model_name
 
-    # Note: The default setting of max_num_seqs (256) and
-    # max_model_len (131072) for this model may cause OOM.
-    # You may lower either to run this example on lower-end GPUs.
 
-    # The configuration below has been confirmed to launch on a single L40 GPU.
-    llm = LLM(
-        model=model_name,
-        max_model_len=4096,
-        max_num_seqs=16,
-        enforce_eager=True, download_dir=cache_dir
-    )
-
+def get_mllama_data(question: str):
     prompt = f"<|image|><|begin_of_text|>{question}"
     stop_token_ids = None
-    return llm, prompt, stop_token_ids
+    return prompt, stop_token_ids
+
 
 
 model_example_map = {
     "llava": run_llava,
-    "llava-next": run_llava_next,
-    "llava-next-video": run_llava_next_video,
-    "llava-onevision": run_llava_onevision,
-    "fuyu": run_fuyu,
     "phi3_v": run_phi3v,
     "paligemma": run_paligemma,
-
     "chameleon": run_chameleon,
-    "minicpmv": run_minicpmv,
-    "blip-2": run_blip2,
-    "internvl_chat": run_internvl,
-    "NVLM_D": run_nvlm_d,
-    "qwen_vl": run_qwen_vl,
     "qwen2_vl": run_qwen2_vl,
     "mllama": run_mllama,
+    "pixtral_hf": run_pixtral_hf,
+    "molmo": run_molmo
 }
+
+model_example_data_map = {
+    "llava": get_llava_data,
+    "phi3_v": get_phi3v_data,
+    "paligemma": get_paligemma_data,
+    "chameleon": get_chameleon_data,
+    "qwen2_vl": get_qwen2_vl_data,
+    "mllama": get_mllama_data,
+    "pixtral_hf": get_pixtral_hf_data,
+    "molmo": get_molmo_data
+}
+
+
+file_name_dict = {
+    "llava": "llava_1_5.csv",
+    "phi3_v": "phi3v.csv",
+    "paligemma": "paligemma.csv",
+    "chameleon": "chameleon.csv",
+    "qwen2_vl": "qwen2_vl.csv",
+    "mllama": "mllama.csv",
+    "pixtral_hf": "pixtral_hf.csv",
+    "molmo": "molmo.csv"
+    }
 
 
 def get_multi_modal_input(args):
@@ -359,23 +209,18 @@ def get_multi_modal_input(args):
     raise ValueError(msg)
 
 
-def main(args):
-    model = args.model_type
-    if model not in model_example_map:
-        raise ValueError(f"Model type {model} is not supported.")
 
-    modality = args.modality
+def run_generation(args, llm, model, model_name, modality):
     mm_input = get_multi_modal_input(args)
+    
     data = mm_input["data"]
     question = mm_input["question"]
+    
+    prompt, stop_token_ids = model_example_data_map[model](question)
 
-    llm, prompt, stop_token_ids = model_example_map[model](question, modality)
-
-    # We set temperature to 0.2 so that outputs can be different
-    # even when all prompts are identical when running batch inference.
     sampling_params = SamplingParams(temperature=0.2,
-                                     max_tokens=args.max_new_tokens,
-                                     stop_token_ids=stop_token_ids)
+                                    max_tokens=args.max_new_tokens,
+                                    stop_token_ids=stop_token_ids)
 
     assert args.num_prompts > 0
     if args.num_prompts == 1:
@@ -419,17 +264,17 @@ def main(args):
     multimodal_request_throughput = args.num_prompts/avg_latency
     
     list_1 = ["Hardware", "Num of Hardware", "Framework", "Model", "Input Length", "Output Length", "Image Size", "Batch Size",
-              "Text Tokens", "Image Tokens", "Multimodal Tokens",
-              "Avg Latency", "Multimodal Token Throughput", "Multimodal Request Throughput"]
+            "Text Tokens", "Image Tokens", "Multimodal Tokens",
+            "Avg Latency", "Multimodal Token Throughput", "Multimodal Request Throughput"]
     
-    list_2 = ["Nvidia A100 GPU", args.num_gpus, "vLLM", args.model, args.max_input_len, args.max_new_tokens, args.image_size, args.num_promptse,
-              text_tokens, image_tokens, multimodal_tokens, 
-              avg_latency, multimodal_token_throughput, multimodal_request_throughput] 
+    list_2 = ["Nvidia A100 GPU", args.num_gpus, "vLLM", model_name, args.max_input_len, args.max_new_tokens, args.image_size, args.num_prompts,
+            text_tokens, image_tokens, multimodal_tokens, 
+            avg_latency, multimodal_token_throughput, multimodal_request_throughput] 
     
 
     assert len(list_1) == len(list_2)
 
-    csv_file = "LangVision_Inference_Bench_vLLM_throughput.csv"
+    csv_file = "Results/" + file_name_dict[args.model_type]
     
     file_exists = os.path.exists(csv_file)
 
@@ -444,11 +289,29 @@ def main(args):
     csvfile.close()
 
 
+def main(args):
+    
+    model = args.model_type
+    if model not in model_example_map:
+        raise ValueError(f"Model type {model} is not supported.")
 
-    for o in outputs:
-        generated_text = o.outputs[0].text
-        print(len(generated_text))
-        print(generated_text)
+    modality = args.modality
+    llm, model_name = model_example_map[model](modality, args.num_gpus)
+
+    for batch_size in [1, 16, 32, 64]:
+        for image_size in [128, 256, 512, 1024]:
+            for input_output_length in [128, 256, 512, 1024]:
+                args.num_prompts    = batch_size
+                args.image_size     = image_size
+                args.max_input_len  = input_output_length
+                args.max_new_tokens = input_output_length
+                run_generation(args, llm, model, model_name, modality)
+
+
+    # for o in outputs:
+    #     generated_text = o.outputs[0].text
+    #     print(len(generated_text))
+    #     print(generated_text)
 
 
 if __name__ == "__main__":
@@ -467,8 +330,9 @@ if __name__ == "__main__":
     parser.add_argument('--num-frames', type=int, default=16, help='Number of frames to extract from the video.')
 
     parser.add_argument('--num-gpus', type=int, default=1, help='Number of GPUs.')
-    parser.add_argument('--warmup-iterations', type=int, default=16, help='Warmup Iterations')
-    parser.add_argument('--eval-iterations', type=int, default=16, help='Evaluating epochs')
+
+    parser.add_argument('--warmup-iterations', type=int, default=3, help='Warmup Iterations')
+    parser.add_argument('--eval-iterations', type=int, default=3, help='Evaluating epochs')
 
     args = parser.parse_args()
     main(args)
